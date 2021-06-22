26ef82401473a45013ae8f3055ac85a4b656a1a6
Joe Sweeney <joe.sweeney224@gmail.com>
2021-06-22 12:01:55 -0600

    playing around, tighter oak/inertia integration

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,31 @@ import {
   login
 } from './Controllers/AuthController.js'
 import State from './State.js'
-import { inertiaRender } from './Middleware/Inertia.js'
+// import { inertiaRender } from './Middleware/Inertia.js'
+import { Inertia } from './Inertia/mod.js'
 
 const app = new Application()
 const store = new WebdisStore({
   url: Deno.env.get('WEBDIS_URL')
 })
 new OakSession(app, store)
+new Inertia(app, /*html*/`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Deno Deploy | CouchDB | Webdis | InertiaJS</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <script type="module" src="http://localhost:3000/@vite/client"></script>
+  <script type="module" src="http://localhost:3000/frontend/app.js"></script>  
+</head>
+<body>
+  @inertia
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>`)
 State.couch = new CouchService(Deno.env.get('COUCH_URL'))
 
 const router = new Router()
@@ -31,12 +49,12 @@ router.get('/register', registerView)
     }
   }).get('/', (ctx) => {
     ctx.response.body = 'hey there, go to /register'
-  }).get('/inertia/1', async (ctx, next) => {
-    await inertiaRender(ctx, next, 'PageOne', {
+  }).get('/inertia/1', (ctx) => {
+    ctx.state.inertia.render('PageOne', {
       hello: 'world'
     })
-  }).get('/inertia/2', async (ctx, next) => {
-    await inertiaRender(ctx, next, 'PageTwo', {
+  }).get('/inertia/2', (ctx) => {
+    ctx.state.inertia.render('PageTwo', {
       hey: 'there'
     })
   })
